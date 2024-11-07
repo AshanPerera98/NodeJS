f745572b0826001dd9c318cbd14b181c9ae6be94
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-07 20:54:29 +0530

    basic routing

--- node-farm/server.js
+++ node-farm/server.js
@@ -1,8 +1,27 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  //   console.log(req);
-  res.end("Hello World!");
+  console.log(req.url);
+
+  switch (req.url) {
+    case "/":
+      res.end("overview");
+      break;
+    case "/overview":
+      res.end("overview");
+      break;
+    case "/product":
+      res.end("product");
+      break;
+    default:
+      res.writeHead(404, {
+        "content-type": "text/html",
+      });
+      res.end("<h1>404</h1><h5>Page not found!</h5>");
+  }
+
+  //   res.end("Hello World!");
 });
 
 server.listen("8080", "127.0.0.1", () => {
