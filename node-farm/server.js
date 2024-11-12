7c79bc3cdd1deba543f5b6c847ac8bcaf24d0833
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-12 13:45:27 +0530

    sending data in the response as application/json

--- node-farm/server.js
+++ node-farm/server.js
@@ -1,5 +1,10 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+
+// This will read the data from the file once when the server starts so it can be read synchronusly
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.stringify({ data });
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -8,12 +13,33 @@ const server = http.createServer((req, res) => {
     case "/":
       res.end("overview");
       break;
+
     case "/overview":
       res.end("overview");
       break;
+
     case "/product":
       res.end("product");
       break;
+
+    case "/api":
+      // Below data read will execute every time a request is made to the endpoint so it needs to be async
+      //
+      // fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      //   const dataObj = JSON.stringify({ data });
+
+      //   res.writeHead(200, {
+      //     "Content-Type": "application/json",
+      //   });
+      //   res.end(dataObj);
+      // });
+
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.end(dataObj);
+      break;
+
     default:
       res.writeHead(404, {
         "content-type": "text/html",
