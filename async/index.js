9195464bf6f4e8b26ecdeb5f3f1b0dab801ae5d2
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-28 08:36:07 +0530

    Simultaneously waiting multiple promises

--- async/index.js
+++ async/index.js
@@ -25,11 +25,29 @@ const writeFilePromise = (file, data) => {
 const dogImage = async () => {
   try {
     const result = await readFilePromise(`${__dirname}/dog.txt`);
-    const res = await superagent.get(
+
+    // waiting for multiple promises
+    const res1Promise = await superagent.get(
       `https://dog.ceo/api/breed/${result}/images/random`
     );
-    console.log(res.body);
-    await writeFilePromise("./dog-image.txt", res.body.message);
+    const res2Promise = await superagent.get(
+      `https://dog.ceo/api/breed/${result}/images/random`
+    );
+    const res3Promise = await superagent.get(
+      `https://dog.ceo/api/breed/${result}/images/random`
+    );
+
+    // resolve all promises
+    const allPromises = await Promise.all([
+      res1Promise,
+      res2Promise,
+      res3Promise,
+    ]);
+
+    // converting results
+    const imageArr = allPromises.map((el) => el.body.message);
+
+    await writeFilePromise("./dog-image.txt", imageArr.join("\n"));
     console.log("Image file saved");
   } catch (err) {
     console.error(err);
