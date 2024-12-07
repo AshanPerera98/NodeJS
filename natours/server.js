73b57d8c3e89cf179a69d47a57d62f7e426ce289
Ashan Perera <ashantiwankaperera@gmail.com>
2024-12-07 21:31:53 +0530

    testTour doc save

--- natours/server.js
+++ natours/server.js
@@ -19,6 +19,32 @@ mongoose
     console.log('DB connection successful!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is mandetory in tour'],
+    unique: true,
+  },
+  rating: { type: Number, default: 1.0 },
+  price: { type: Number, required: [true, 'Price is mandetory in tour'] },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'Test tour from mongoose',
+  rating: 4.7,
+  price: 13.99,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
