f03b79de5953b22023f6d788ea4891efd946e9ad
Ashan Perera <ashantiwankaperera@gmail.com>
2024-12-07 21:11:53 +0530

    mongoDB connection

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); // set the path for config file iin dotenv
 
 const app = require('./app');
+
+const DB = process.env.DATABASE_CONNECTION_STRING.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT || 3000;
 
