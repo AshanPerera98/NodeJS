c43832a08341957bafd7db4fbd45e88a643615db
Ashan Perera <ashantiwankaperera@gmail.com>
2024-12-16 14:50:18 +0530

    virtual properties

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,58 +1,70 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is mandetory in tour'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is mandetory in tour'],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      reuired: [true, 'Duration is required'],
+    },
+    maxGroupSize: {
+      type: Number,
+      reuired: [true, 'Group size is required'],
+    },
+    difficulty: {
+      type: String,
+      reuired: [true, 'Difficulty is required'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is mandetory in tour'],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Summary is required'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Cover image is required'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // this will make this field not visible to client
+    },
+    startingDates: [Date],
   },
-  duration: {
-    type: Number,
-    reuired: [true, 'Duration is required'],
-  },
-  maxGroupSize: {
-    type: Number,
-    reuired: [true, 'Group size is required'],
-  },
-  difficulty: {
-    type: String,
-    reuired: [true, 'Difficulty is required'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Price is mandetory in tour'],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Summary is required'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Cover image is required'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, // this will make this field not visible to client
-  },
-  startingDates: [Date],
+  {
+    // each time the schema is converted to JSON and object we need virtuals as well
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// virtual property is a derived property from the actual data
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
