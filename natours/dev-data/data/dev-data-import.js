ef221ba96078d9bd7d4eaf513e3c6c7a014eeeb6
Ashan Perera <ashantiwankaperera@gmail.com>
2024-12-30 10:00:25 +0530

    importing full data set

--- natours/dev-data/data/dev-data-import.js
+++ natours/dev-data/data/dev-data-import.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -21,10 +23,16 @@ mongoose
   });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data deleted');
   } catch (err) {
     console.error(err);
@@ -35,6 +43,8 @@ const deleteData = async () => {
 const insertData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log(`Tours created`);
   } catch (err) {
     console.log(err);
