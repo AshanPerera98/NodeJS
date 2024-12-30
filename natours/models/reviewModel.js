62b982aca82cd58b18209af40af24d0f4c65b532
Ashan Perera <ashantiwankaperera@gmail.com>
2024-12-30 12:50:57 +0530

    calculating avaerate ratings and number of ratings in tour

--- natours/models/reviewModel.js
+++ natours/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -45,6 +46,62 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+// static method, static methods can be executed in Models so that aggregate pipeline can be utilized
+reviewSchema.statics.calculateAverageRating = async function (tourId) {
+  // "this" keyword points to the model
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        numberOfRatings: { $sum: 1 },
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  // "stats" returns an array with one object containing the results
+  const { ratingsAverage, ratingsQuantity } = stats[0];
+
+  //   Update the relevant tour with calculated stats
+  if (stats.length > 0)
+    await Tour.findByIdAndUpdate(tourId, { ratingsAverage, ratingsQuantity });
+  //   defaults
+  else
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+    });
+};
+
+// document middleware to execute static method after saving
+reviewSchema.post('save', function () {
+  // "this.constructor" will point to the model that the document is created
+  // "this" points to the current doc
+  this.constructor.calculateAverageRating(this.tour);
+});
+
+// findOneAndUpdate
+// findOneAndDelete
+// cannot use documnet middleware in update and delete so have to use pre query middleware
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // query middleware doesnt have access to the document so query ("this") needs to be executed to get the document
+  // cannot do the calculation at this poit because current query ("this") returns the document before doing the change beacuse this is a PRE middleware
+  // Adding the result documnet to the query to be used in post middleware
+  this.alteringReview = this.findOne();
+  next();
+});
+
+// post query middlware to add the stats to the tour
+reviewSchema.post(/^findOneAnd/, async function () {
+  // "this.alteringReview.constructor" will point to the Model which is used to create the documnet
+  await this.alteringReview.constructor.calculateAverageRating(
+    this.alteringReview.tour
+  );
 });
 
 const Review = mongoose.model('Review', reviewSchema);
