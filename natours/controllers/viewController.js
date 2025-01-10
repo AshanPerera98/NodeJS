a9402a869d688ca738e346f6b59c74693eb1ed5f
Ashan Perera <ashantiwankaperera@gmail.com>
2025-01-10 12:18:02 +0530

    rendering tour with data pug template

--- natours/controllers/viewController.js
+++ natours/controllers/viewController.js
@@ -1,9 +1,15 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'All Tours' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  // Get the data for the tour (including guides and reviews)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    feilds: 'review rating user',
+  });
+
+  res.status(200).render('tour', { title: tour.title, tour });
+});
 
 exports.getOverview = catchAsync(async (req, res) => {
   // get tour data from DB
